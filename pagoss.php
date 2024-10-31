82900056f3dafc5f12174100c16d12399d2aa3bd
jeremyjdm <jeremyjdm04@gmail.com>
2024-10-30 18:59:47 -0600

    Update pagoss.php

--- pagoss.php
+++ pagoss.php
@@ -7,7 +7,7 @@ if ($_SESSION['perfil'] !== 'secretaria') {
 
 $servername = "localhost";
 $username = "root";
-$password = "root1";
+$password = "root";
 $dbname = "bdtienda";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
